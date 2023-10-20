66f50735bb2be659024135c5ba4f768032c5708e
Mostafa Moharram <mostafa.moharm2001@gmail.com>
2023-10-20 06:59:32 +0300

    Add Home View and Book View

--- BulkyWeb/Areas/Customer/Controllers/HomeController.cs
+++ BulkyWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using BulkyBook.Models;
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -8,15 +9,23 @@ namespace BulkyBookWeb.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            return View(_unitOfWork.ProductRepository.GetAll(new[] { "Category" }));
+        }
+
+        public IActionResult Details(int id)
+        {
+            var product = _unitOfWork.ProductRepository.Get(product => product.Id == id, new[] { "Category" });
+            return View(product);
         }
 
         public IActionResult Privacy()
